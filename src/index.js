6ad4bfd9155cb86f3aba3902b6f5c1902ffb294b
ziauddinziya07 <ziauddinziya07@gmail.com>
2022-03-05 10:41:25 +0530

    added firebase supported files

--- src/index.js
+++ src/index.js
@@ -1,9 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import db from "./util/firebase";
+import { uid } from "uid";
+import { set, ref, push } from "firebase/database";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
 var openModalBtn = document.getElementById("open-modal-btn");
 openModalBtn.click();
 openModalBtn.style.display = "none";
+
+const appName = "khalid-weds-shireen";
+// writing to Database
+function write(data) {
+    // const uuid = uid(32);
+    // console.log(uuid);
+    // set(ref(db, `/${appName}/${uuid}`), {
+    //     data,
+    // });
+    push(ref(db, `/${appName}/`), 
+        data,
+    ).then((val) => {
+        console.log(val._path.pieces_[1]);
+    });
+}
+
+write({ name: "Ziya" });
